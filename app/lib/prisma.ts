b64d33aca7bc855878c44f71682dea59020e70ae
Eric K <ericskao@gmail.com>
2024-07-13 15:09:52 -0700

    update eslint

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -8,9 +8,9 @@ const prismaClientSingleton = () => {
   return new PrismaClient({ adapter });
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
